e5053eddffdc037ff1d40ca31dc66bfc71fb965e
Faraz Najam <faraz_najam@windows.com>
2026-03-17 22:40:44 +1100

    Initial Terraform architecture deployment

--- infra/network.tf
+++ infra/network.tf
@@ -10,6 +10,7 @@ resource "azurerm_public_ip" "public_ip" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_subnet" "subnets" {
