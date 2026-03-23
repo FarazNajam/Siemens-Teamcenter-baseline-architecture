ad49d1d3a47b399e0c925a7c650988436c4f87b9
Faraz Najam <faraz_najam@windows.com>
2026-03-23 22:37:43 +1100

    Initial Terraform architecture deployment

--- infra/main.tf
+++ infra/main.tf
@@ -11,71 +11,71 @@ locals {
   }
 }
 
-resource "azurerm_application_gateway" "appgw" {
-  name                = var.appgw
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+#resource "azurerm_application_gateway" "appgw" {
+#  name                = var.appgw
+#  resource_group_name = azurerm_resource_group.rg.name
+#  location            = azurerm_resource_group.rg.location
 
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 1
-  }
+#  sku {
+#    name     = "Standard_v2"
+#    tier     = "Standard_v2"
+#    capacity = 1
+#  }
 
-  gateway_ip_configuration {
-    name      = local.appgw.gateway_ip_config
-    subnet_id = azurerm_subnet.subnets["appgw-subnet"].id
-  }
+#  gateway_ip_configuration {
+#    name      = local.appgw.gateway_ip_config
+#    subnet_id = azurerm_subnet.subnets["appgw-subnet"].id
+#  }
 
-  frontend_port {
-    name = local.appgw.frontend_port
-    port = var.frontend_port
-  }
+#  frontend_port {
+#    name = local.appgw.frontend_port
+#    port = var.frontend_port
+#  }
 
-  frontend_ip_configuration {
-    name                 = local.appgw.frontend_ip_config
-    public_ip_address_id = azurerm_public_ip.public_ip.id
-  }
+#  frontend_ip_configuration {
+#    name                 = local.appgw.frontend_ip_config
+#    public_ip_address_id = azurerm_public_ip.public_ip.id
+#  }
 
-  backend_address_pool {
-    name = local.appgw.backend_pool
+#  backend_address_pool {
+#    name = local.appgw.backend_pool
 
-  }
+#  }
 
-  backend_http_settings {
-    name                  = local.appgw.backend_settings
-    cookie_based_affinity = "Disabled"
-    port                  = 443
-    protocol              = "Https"
-    request_timeout       = 60
-  }
+#  backend_http_settings {
+#    name                  = local.appgw.backend_settings
+#    cookie_based_affinity = "Disabled"
+#    port                  = 443
+#    protocol              = "Https"
+#    request_timeout       = 60
+#  }
 
-  http_listener {
-    name                           = local.appgw.listener
-    frontend_ip_configuration_name = local.appgw.frontend_ip_config
-    frontend_port_name             = local.appgw.frontend_port
-    protocol                       = "Https"
-    ssl_certificate_name           = "appgw-cert"
-  }
+#  http_listener {
+#    name                           = local.appgw.listener
+#    frontend_ip_configuration_name = local.appgw.frontend_ip_config
+#    frontend_port_name             = local.appgw.frontend_port
+#    protocol                       = "Https"
+#    ssl_certificate_name           = "appgw-cert"
+#  }
 
-  ssl_policy {
-    policy_type = "Predefined"
-    policy_name = "AppGwSslPolicy20220101"
-  }
+#  ssl_policy {
+#    policy_type = "Predefined"
+#    policy_name = "AppGwSslPolicy20220101"
+#  }
 
-  ssl_certificate {
-    name     = "appgw-cert"
-    data     = filebase64("${path.module}/cert.pfx")
-    password = var.cert_password
-  }
+#  ssl_certificate {
+#    name     = "appgw-cert"
+#    data     = filebase64("${path.module}/cert.pfx")
+#    password = var.cert_password
+#  }
 
-  request_routing_rule {
-    name                       = local.appgw.routing_rule
-    priority                   = 9
-    rule_type                  = "Basic"
-    http_listener_name         = local.appgw.listener
-    backend_address_pool_name  = local.appgw.backend_pool
-    backend_http_settings_name = local.appgw.backend_settings
-  }
-}
+#  request_routing_rule {
+#    name                       = local.appgw.routing_rule
+#    priority                   = 9
+#    rule_type                  = "Basic"
+#    http_listener_name         = local.appgw.listener
+#    backend_address_pool_name  = local.appgw.backend_pool
+#    backend_http_settings_name = local.appgw.backend_settings
+#  }
+#}
 
